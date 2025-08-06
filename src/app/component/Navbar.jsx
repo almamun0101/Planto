a1d5f38951adae340d5166e6ff06c3b50fa6424f
Al Mamun Khan <almamunkhan0101@gmail.com>
2025-08-06 13:42:37 +0600

    project based start

---
+++ src/app/component/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="container">
+        <nav>
+          <img src="/logo.png" alt="logo" />
+          <ul>
+            <li>
+              <a href="">Home</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
